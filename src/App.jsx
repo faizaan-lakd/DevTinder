21d636ae6551a6e3cd3a25b81a624d7e8c0fd026
Faizaan <faizaan.lakd@gmail.com>
2025-04-21 06:26:17 -0400

    added routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
-import Navbar from "./component/Navbar";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Body from "./component/Body";
+import Login from "./component/Login";
+import Profile from "./component/Profile";
 
 function App() {
   return (
     <>
-      <Navbar />
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }

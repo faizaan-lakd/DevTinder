fe2ee4ab27f04d2975a074383906935c304bd90e
Faizaan <faizaan.lakd@gmail.com>
2025-10-05 18:49:55 -0400

    add fetch and display user card on feed page

--- src/component/Feed.jsx
+++ src/component/Feed.jsx
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { BASE_URL } from "../utils/constants";
+import axios from "axios";
+import { addFeed } from "../utils/feedSlice";
+import UserCard from "./UserCard";
 
 const Feed = () => {
-  return <div>Feed</div>;
+  const feed = useSelector((store) => store.feed);
+  const dispatch = useDispatch();
+
+  const getFeed = async () => {
+    if (feed) {
+      return;
+    }
+
+    try {
+      const res = await axios.get(BASE_URL + "/feed", {
+        withCredentials: true,
+      });
+      dispatch(addFeed(res?.data?.data));
+    } catch {
+      // TODO add error page.
+    }
+  };
+
+  useEffect(() => {
+    getFeed();
+  }, []);
+
+  return (
+    feed && (
+      <div className="flex justify-center items-center min-h-[80vh]">
+        <UserCard user={feed[0]} />
+      </div>
+    )
+  );
 };
 
 export default Feed;
